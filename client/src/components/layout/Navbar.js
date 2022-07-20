1ad91c6bf1ad6fcc1bf5f3c746a31acbc587b68b
mykullma <752830132@qq.com>
2022-07-20 17:36:52 +0800

    components for landing, register & login

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { logout } from '../../actions/auth';
+
+const Navbar = ({ logout, auth: { isAuthenticated, loading } }) => {
+  const guestLinks = (
+    <ul>
+      <li><Link to="profiles">Developers</Link></li>
+      <li><Link to="register">Register</Link></li>
+      <li><Link to="login">Login</Link></li>
+    </ul>
+  );
+  const authLinks = (
+    <ul>
+      <li><Link to="#!" onClick={logout}><i className="fa fa-sign-out-alt"></i> Logout</Link></li>
+    </ul>
+  );
+  
+  return (
+    <nav className="navbar bg-dark">
+      <h1>
+        <Link to="/"><i className="fa fa-code"></i> DevConnector</Link>
+      </h1>
+      { !loading && ( isAuthenticated ? authLinks : guestLinks ) }
+    </nav>
+  )
+};
+
+const useState = state => ({
+  auth: state.auth
+});
+
+export default connect(useState, { logout })(Navbar);
