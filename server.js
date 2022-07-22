5457a8628002c556b5e4af65acec23926112efda
mykullma <752830132@qq.com>
2022-07-22 18:07:48 +0800

    production

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
-const express = require('express');
-const connectDB = require('./config/db')
+import express from 'express';
+import { resolve } from 'path';
+import connectDB from './config/db';
 
-const app = express();
-const port = process.env.PORT || 5000;
 connectDB();
+const app = express();
 app.use(express.json({ extended: false }));
 
 // routes
-app.get('/', (req, res) => res.send('api running'));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+      res.sendFile(resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`listening on port ${port}`));
